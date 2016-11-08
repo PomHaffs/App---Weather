a65d18deed7d3247dac2d4b6919f67b3afe12bf9
PomHaffs <tomashaffenden@icloud.com>
2016-11-08 16:52:59 +1100

    basic UI complete

---
+++ WeatherApp/WeatherVC.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherVC.swift
+//  WeatherApp
+//
+//  Created by Tomas-William Haffenden on 8/11/16.
+//  Copyright © 2016 PomHaffs. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
