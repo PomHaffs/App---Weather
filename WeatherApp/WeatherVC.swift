866130539cd68b92c17907ece9cbffe4d0c0601c
PomHaffs <tomashaffenden@icloud.com>
2016-11-09 12:30:07 +1100

    added cell delegate/data code

--- WeatherApp/WeatherVC.swift
+++ WeatherApp/WeatherVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class WeatherVC: UIViewController {
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
 
     @IBOutlet weak var dateLabel: UILabel!
@@ -21,8 +21,31 @@ class WeatherVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //this tells comp where to find the dependancies above
+        tableView.delegate = self
+        tableView.dataSource = self
 
     }
-
+    
+    //this is need for all tableView things 1 of 3
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    //this is 2 of 3
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    //this is 3 of 3 - creates generic cell for duplication
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //ID MUST link to table ID in storyboard
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+    
+    
 }
 
